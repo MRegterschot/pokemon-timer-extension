819b984006ac70ff7bef3faa6f3160528dbdf94e
Marijn Regterschot <marijn.regterschot@gmail.com>
2023-08-15 18:40:50 +0200

    feat: timer

---
+++ pokemon.js
@@ -0,0 +1,66 @@
+console.log("pokemon.js loaded");
+
+let timerDiv;
+let parentDiv;
+const backend_url = "https://poketwitch.bframework.de/";
+
+function addTimerDiv() {
+  parentDiv = document.getElementsByClassName(
+    "chat-input__buttons-container"
+  )[0];
+
+  timerDiv = document.createElement("div");
+  timerDiv.setAttribute("id", "timerDiv");
+  timerDiv.setAttribute(
+    "style",
+    "display: flex; flex-direction: row; justify-content: center; align-items: center; width: 100%; color: var(--color-text-base) !important;"
+  );
+
+  parentDiv.insertBefore(timerDiv, parentDiv.children[1]);
+}
+
+
+function str_pad_left(string, pad, length) {
+  return (new Array(length + 1).join(pad) + string).slice(-length);
+}
+
+function mainloop() {
+  fetch(backend_url + "info/events/last_spawn/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var next_spawn = data.next_spawn;
+
+      function cooldown_wait() {
+        next_spawn -= 1;
+
+        var minutes = Math.floor(next_spawn / 60);
+        var seconds = next_spawn - minutes * 60;
+
+        if (
+          document.getElementsByClassName("chat-input__buttons-container")[0]
+            .children[1] != timerDiv
+        ) {
+          addTimerDiv();
+        }
+
+        timerDiv.innerHTML =
+          str_pad_left(minutes, "0", 2) + ":" + str_pad_left(seconds, "0", 2);
+        if (next_spawn > 0) {
+          setTimeout(function () {
+            cooldown_wait();
+          }, 1000);
+        } else {
+          setTimeout(function () {
+            mainloop();
+          }, 2000);
+        }
+      }
+
+      cooldown_wait();
+    });
+}
+
+addTimerDiv();
+mainloop();
