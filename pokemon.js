adb078bac69d3ba6dc8eee9666272026021df9aa
Marijn Regterschot <marijn.regterschot@gmail.com>
2023-08-16 22:32:15 +0200

    fix: timer now works even when switching tabs

--- pokemon.js
+++ pokemon.js
@@ -4,10 +4,18 @@ let timerDiv;
 let parentDiv;
 const backend_url = "https://poketwitch.bframework.de/";
 
+// browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   console.log("tab updated");
+// });
+
 function addTimerDiv() {
   parentDiv = document.getElementsByClassName(
     "chat-input__buttons-container"
   )[0];
+
+  if (parentDiv == undefined) {
+    return;
+  }
 
   timerDiv = document.createElement("div");
   timerDiv.setAttribute("id", "timerDiv");
@@ -18,7 +26,6 @@ function addTimerDiv() {
 
   parentDiv.insertBefore(timerDiv, parentDiv.children[1]);
 }
-
 
 function str_pad_left(string, pad, length) {
   return (new Array(length + 1).join(pad) + string).slice(-length);
@@ -35,18 +42,23 @@ function mainloop() {
       function cooldown_wait() {
         next_spawn -= 1;
 
-        var minutes = Math.floor(next_spawn / 60);
-        var seconds = next_spawn - minutes * 60;
-
         if (
-          document.getElementsByClassName("chat-input__buttons-container")[0]
-            .children[1] != timerDiv
+          document.getElementsByClassName("chat-input__buttons-container")[0] !=
+          undefined
         ) {
-          addTimerDiv();
+          if (
+            document.getElementsByClassName("chat-input__buttons-container")[0]
+              .children[1] != timerDiv
+          ) {
+            addTimerDiv();
+          }
+
+          var minutes = Math.floor(next_spawn / 60);
+          var seconds = next_spawn - minutes * 60;
+          timerDiv.innerHTML =
+            str_pad_left(minutes, "0", 2) + ":" + str_pad_left(seconds, "0", 2);
         }
 
-        timerDiv.innerHTML =
-          str_pad_left(minutes, "0", 2) + ":" + str_pad_left(seconds, "0", 2);
         if (next_spawn > 0) {
           setTimeout(function () {
             cooldown_wait();
